c010ea349bb1e08432acb2ad7258917d83424799
Omarmedhat0 <omar.medhat9898@gmail.com>
2024-04-30 00:03:56 +0300

    First Version of Test Cases

---
+++ src/DemoScript.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestScenarios(unittest.TestCase):
+    def test_Senario1(self):
+        # ReqsService(ID,Port,Pin)
+        # ReqsService(ID,Port,Pin,Value)
+        self.assertEqual(value, 3)
+if __name__ == '__main__':
+    unittest.main()
